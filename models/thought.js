4fc733b2f3d804a1aaab183c377e5d76c98d047d
Amal Hirsi <ahirsi@hotmail.co.uk>
2022-11-05 15:46:10 +0000

    First schema

---
+++ models/thought.js
@@ -0,0 +1,67 @@
+const { Schema, Types } = require('mongoose');
+const thoughtSchema = require('./thought');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1, 
+            max_length: 280,
+          },
+
+          createdAt: {
+            type: date,
+            default: Date.now,
+        
+          },
+
+         userName: {
+        type: String,
+        required: true,
+      },
+
+      reactions: [reactionSchema]
+
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }  
+);
+
+    const reactionSchema = new Schema(
+        {
+            reactionId: {
+                type: Schema.Types.ObjectId,
+                Default: 
+              },
+    
+              reactionBody: {
+                type: String,
+                required: true,
+                min_length: 1, 
+                max_length: 280,
+              },
+                    userName: {
+                        type: String,
+                        required: true,
+                      },
+            
+                      createdAt: {
+                        type: Date,
+                        default: Date.now,
+                        get:
+                      },
+
+                      {
+                    toJSON: {
+                        getters: true,
+                         virtuals: true},
+                },
+                id: false,
+                }
+                );
+    
+    module.exports = thoughtSchema;
+    
